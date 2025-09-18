11658410c63a80aa0cf41c58cacb799ecc1746dd
mattm3192 <mattm3192@gail.com>
2025-09-17 21:53:51 -0500

    CH1 L4 completed. Added ability to except a prompt as an argument.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,8 +10,12 @@ from google import genai
 client = genai.Client(api_key=api_key)
 
 def main():
+    args = sys.argv
+    if len(args) < 2:
+        print("Prompt not included")
+        sys.exit(1)
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=args[1]
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
