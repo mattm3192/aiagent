34fdb983ef9afdad205515674022c5e183c95a70
mattm3192 <mattm3192@gail.com>
2025-09-18 00:49:07 -0500

    Ch1 L5, completed. Updated code to store user prompt in list of messages using the content type from genai.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
@@ -14,8 +15,12 @@ def main():
     if len(args) < 2:
         print("Prompt not included")
         sys.exit(1)
+    user_prompt = args[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=args[1]
+        model='gemini-2.0-flash-001', contents=messages,
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
