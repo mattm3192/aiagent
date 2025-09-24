26d200b2eb02addac28d86cacc37f8004913b051
mattm3192 <mattm3192@gail.com>
2025-09-23 19:22:54 -0500

    completed CH2 L1: added files for calculator code and associated tests.

--- main.py
+++ main.py
@@ -22,9 +22,13 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', contents=messages,
     )
+    if len(args) > 2:
+        if args[2] == "--verbose":
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
 
 
 if __name__ == "__main__":
