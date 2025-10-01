d3b7fa23f3bd8e06f7b19e7a30b75c02ceb3054f
mattm3192 <mattm3192@gail.com>
2025-09-30 20:48:07 -0500

    completed ch3 l4: enabling calling of the functions for the ai agent.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import *
 
 def main():
     load_dotenv()
@@ -31,10 +31,13 @@ def main():
         print(f"User prompt: {user_prompt}\n")
 
     messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),   
     ]
+#build loop here, limit 20, use try-except block
+# after each call check if response.text is returned if so break loop
 
     generate_content(client, messages, verbose)
+
     
 def generate_content(client, messages, verbose):
     model_name = 'gemini-2.0-flash-001'
@@ -46,15 +49,32 @@ def generate_content(client, messages, verbose):
 	    tools=[available_functions], system_instruction=system_prompt,
         )
     )
+    for candidate in response.candidates:
+        messages.append(candidate.content)
+    #put candidate code here
+    #followed by conversion to type.content and append to messages
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response: ")
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-    else:
-        print(response.text)
+    
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+   
     
 
 
--- tests.py
+++ tests.py
@@ -1,19 +1,19 @@
 from functions.run_pyhton_file import run_python_file
 
 def test():
-    result = run_python_file("calculator", "main.py")
+    result = run_python_file("./calculator", "main.py")
     print(result)
     
-    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    result = run_python_file("./calculator", "main.py", ["3 + 5"])
     print(result)
     
-    result = run_python_file("calculator", "tests.py")
+    result = run_python_file("./calculator", "tests.py")
     print(result)
     
-    result = run_python_file("calculator", "../main.py")
+    result = run_python_file("./calculator", "../main.py")
     print(result)
     
-    result = run_python_file("calculator", "nonexistent.py")
+    result = run_python_file("./calculator", "nonexistent.py")
     print(result)
 
 if __name__ == "__main__":
